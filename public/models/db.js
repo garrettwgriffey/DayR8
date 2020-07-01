ed7228573cb2b6150fcfa0a68ce8ab146b537597
Theresa E <t.eatherly@gmail.com>
2020-06-30 19:57:42 -0500

    sql database/schema created and dropped

---
+++ public/models/db.js
@@ -0,0 +1,23 @@
+const mysql = require("mysql");
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "password",
+    database: "feelings"
+  });
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    connection.end();
+  });
+  
---
+++ public/models/feelings.sql
@@ -0,0 +1,9 @@
+Use feelings;
+
+INSERT INTO feelings (title, emotion, notes)
+VALUES ("Today", "happy", "I felt up because this brightened my day");
+
+INSERT INTO feelings (title, emotion, notes)
+VALUES ("Yesterday", "anxious", "My anxiety was triggered by Karen's actions");
+
+
