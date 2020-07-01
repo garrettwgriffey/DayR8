7ee4da8ccd5991421d0df6011b1f5a93fcc1c89a
Timothy Mickiewicz <timothy.mickiewicz@gmail.com>
2020-06-30 23:36:30 -0500

    heroku deployment successful

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
+// match one above, send back our index.html file.
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
