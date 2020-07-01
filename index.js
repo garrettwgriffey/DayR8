4775cb364891a91a29f8f4f262137a61bb3c7472
Timothy Mickiewicz <timothy.mickiewicz@gmail.com>
2020-06-30 23:28:06 -0500

    changed port var

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ app.get('*', (req, res) => {
 
 // set port, listen for requests
 const port = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
 });
