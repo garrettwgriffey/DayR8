d3392fcb90d1775e5b5e3d0fbc71c123e9f6c0b1
charlenne alexander <charlennep@gmail.com>
2020-07-07 18:57:16 -0500

    added .env with the haws db url and edit styling on hotline.

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require("dotenv").config();
 module.exports = {
   development: {
     username: "root",
@@ -6,25 +6,25 @@ module.exports = {
     database: "dayr8",
     host: "localhost",
     port: 3306,
-    dialect: "mysql"
+    dialect: "mysql",
   },
   test: {
-    username: 'root',
+    username: "root",
     password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
+    database: "database_test",
+    host: "127.0.0.1",
     port: 3306,
-    dialect: 'mysql'
+    dialect: "mysql",
   },
   production: {
-    // "use_env_variable": "JAWSDB_URL",
-    // "dialect": "mysql"
+    use_env_variable: "JAWSDB_URL",
+    dialect: "mysql",
 
-    username: process.env.JAWS_USERNAME,
-    password: process.env.JAWS_PASSWORD,
-    database: process.env.JAWS_DATABASE,
-    host: process.env.JAWS_HOST,
-    port: process.env.JAWS_PORT,
-    dialect: process.env.JAWS_DIALECT
-  }
-}
+    // username: process.env.JAWS_USERNAME,
+    // password: process.env.JAWS_PASSWORD,
+    // database: process.env.JAWS_DATABASE,
+    // host: process.env.JAWS_HOST,
+    // port: process.env.JAWS_PORT,
+    // dialect: process.env.JAWS_DIALECT
+  },
+};
