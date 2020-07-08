d19f7e121e158a8650d165f2be5eb59103c62d21
Theresa E <t.eatherly@gmail.com>
2020-07-07 20:15:53 -0500

    deleted require(mySql) functions/ not needed for react app

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,4 +1,3 @@
-// const mysql = require("mysql");
 const db = require('../models')
 
 const seeds = [
@@ -15,29 +14,3 @@ for (let i = 0; i < seeds.length; i++) {
 }
 
 
-  // var connection = mysql.createConnection(
-//   process.env.NODE_ENV === "production" ? process.env.JAWSDB_URL :
-// {
-//     host: "localhost",
-  
-//     // Your port; if not 3306
-//     port: 3306,
-  
-//     // Your username
-//     user: "root",
-  
-//     // Your password
-//     password: "password",
-//     database: "dayr8"
-//   });
-  
-//   connection.connect(function(err) {
-//     if (err) throw err;
-//     console.log("connected as id " + connection.threadId);
-//     connection.query("INSERT INTO feelings (`title`, `emotion`, `notes`) VALUES ('Today', 'happy', 'I felt up because this brightened my day') , ('Yesterday', 'anxious', 'My anxiety was triggered by Karens actions')" ,
-//       (err, results) => {
-//       if (err) throw (err); 
-//       console.log(results)
-//         connection.end();
-//      }
-// )});
