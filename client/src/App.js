b0ec8b8e4e23fcb0959840d1d83738c2b186db99
Timothy Mickiewicz <timothy.mickiewicz@gmail.com>
2020-07-09 22:55:53 -0500

    created privateroute.js to handle page viewing, validate function to authenticate, hook on app.js to get credentials

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/layout/NavBar";
 import Footer from "./components/layout/Footer";
@@ -6,15 +6,29 @@ import SignIn from "./components/pages/SignIn";
 import SignUp from "./components/pages/SignUp";
 import Note from "./components/pages/Note";
 import "./App.css";
+import {PrivateRoute} from './util/PrivateRoute';
+import { Validate } from './util/Validate';
 
 function App() {
+  const [user, setUser] = useState("");
+  useEffect(() => {
+    const sendValidation = async () => {
+        try {
+            await Validate().then((res) => {setUser(res); console.log(user)});
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    sendValidation();
+});
   return (
     <Router>
       <NavBar />
-      <Switch>
-        {/* <Route exact path="/" component={SignIn} />
-        <Route path="/signup" component={SignUp} /> */}
-        <Note />
+        <Switch>
+          <Route exact path="/" component={SignIn} />
+          <Route exact path="/signup" component={SignUp} />
+          <PrivateRoute exact path="/note" component={Note} />
+          {/* <Route exact component={NoMatch} /> */}
       </Switch>
       <Footer />
     </Router>
