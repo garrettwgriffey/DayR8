350977f2669b8b6ee4b8013c7b61f4b937fd9ef6
charlenne alexander <charlennep@gmail.com>
2020-07-14 19:10:35 -0500

    added validator and added the user to the model feelings

--- config/passport.js
+++ config/passport.js
@@ -1,42 +1,48 @@
-var passport = require('passport')
-var LocalStrategy = require('passport-local').Strategy
-var db = require('../models')
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+var db = require("../models");
+var validator = require("validator");
 
-passport.use('local', new LocalStrategy({
-  // User signs in with a username and password
-  username: 'username'
-},
-function (username, password, done) {
-  // When a user tries to sign in this code runs
-  db.User.findOne({
-    where: {
-      username: username
+passport.use(
+  "local",
+  new LocalStrategy(
+    {
+      // User signs in with a username and password
+      username: "username",
+    },
+    function (username, password, done) {
+      // When a user tries to sign in this code runs
+      db.User.findOne({
+        where: {
+          username: username,
+        },
+      }).then(function (dbUser) {
+        console.log("running findOne");
+        // If there's no user with the given username
+        if (!dbUser) {
+          console.log("no user");
+          return done(null, false, {
+            message: "Incorrect Username!",
+          });
+        } else if (!dbUser.validPassword(password)) {
+          console.log("incorrect password");
+          return done(null, false, {
+            message: "Incorrect Password!",
+          });
+        }
+        // If none of the above, return the user
+        return done(null, dbUser);
+      });
     }
-  }).then(function (dbUser) {
-    console.log("running findOne")
-    // If there's no user with the given username
-    if (!dbUser) {
-      console.log("no user");
-      return done(null, false, {
-        message: 'Incorrect Username!'
-      })
-    } else if (!dbUser.validPassword(password)) {
-      console.log("incorrect password");
-      return done(null, false, {
-        message: 'Incorrect Password!'
-      })
-    }
-    // If none of the above, return the user
-    return done(null, dbUser)
-  })
-}))
+  )
+);
 
 passport.serializeUser(function (user, cb) {
-  cb(null, user)
-})
+  cb(null, user);
+});
 
 passport.deserializeUser(function (obj, cb) {
-  cb(null, obj)
-})
+  cb(null, obj);
+});
 
-module.exports = passport
+module.exports = passport;
