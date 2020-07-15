d3f4cb987c7c7d55e590ba852f37b114246db506
garrett griffey <garrettwgriffey@gmail.com>
2020-07-15 17:58:14 -0500

    changed x axis to date

--- client/src/components/Plot.js
+++ client/src/components/Plot.js
@@ -6,23 +6,23 @@ function MyChart() {
       () => [
         {
           label: 'Series 1',
-          data: [{ x: 1, y: 1 }, { x: 2, y: 5 }, { x: 3, y: 10 }]
+          data: [{ x: new Date(2020,7,13), y: 1 }, { x: new Date(2020,7,14), y: 5 }, { x: new Date(2020,7,15), y: 10 }]
         },
-        {
-          label: 'Series 2',
-          data: [{ x: 1, y: 7 }, { x: 2, y: 8 }, { x: 3, y: 3 }]
-        },
-        {
-          label: 'Series 3',
-          data: [{ x: 1, y: 2 }, { x: 2, y: 4 }, { x: 3, y: 9 }]
-        }
+        // {
+        //   label: 'Series 2',
+        //   data: [{ x: 1, y: 7 }, { x: 2, y: 8 }, { x: 3, y: 3 }]
+        // },
+        // {
+        //   label: 'Series 3',
+        //   data: [{ x: 1, y: 2 }, { x: 2, y: 4 }, { x: 3, y: 9 }]
+        // }
       ],
       []
     )
    
     const axes = React.useMemo(
       () => [
-        { primary: true, type: 'linear', position: 'bottom' },
+        { primary: true, type: 'utc', position: 'bottom' },
         { type: 'linear', position: 'left' }
       ],
       []
