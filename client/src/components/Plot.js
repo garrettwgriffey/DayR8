7bac9783b3bda7d23552341b675fd8d20891ad52
garrett griffey <garrettwgriffey@gmail.com>
2020-07-15 17:43:55 -0500

    react charts rendering

--- client/src/components/Plot.js
+++ client/src/components/Plot.js
@@ -1,1 +1,43 @@
-// import React from 'react';
+import React from "react";
+import { Chart } from "react-charts";
+
+function MyChart() {
+    const data = React.useMemo(
+      () => [
+        {
+          label: 'Series 1',
+          data: [{ x: 1, y: 1 }, { x: 2, y: 5 }, { x: 3, y: 10 }]
+        },
+        {
+          label: 'Series 2',
+          data: [{ x: 1, y: 7 }, { x: 2, y: 8 }, { x: 3, y: 3 }]
+        },
+        {
+          label: 'Series 3',
+          data: [{ x: 1, y: 2 }, { x: 2, y: 4 }, { x: 3, y: 9 }]
+        }
+      ],
+      []
+    )
+   
+    const axes = React.useMemo(
+      () => [
+        { primary: true, type: 'linear', position: 'bottom' },
+        { type: 'linear', position: 'left' }
+      ],
+      []
+    )
+   
+    return (
+      <div
+        style={{
+          width: '400px',
+          height: '300px'
+        }}
+      >
+        <Chart data={data} axes={axes} />
+      </div>
+    )
+  }
+
+export default MyChart;
